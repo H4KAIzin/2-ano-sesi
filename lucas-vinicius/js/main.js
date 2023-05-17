116cd7308db3db8ce642dca931508285d8629d81
H4KAIzin <131922049+H4KAIzin@users.noreply.github.com>
2023-05-17 08:47:45 -0300

    Add files via upload

--- lucas-vinicius/js/main.js
+++ lucas-vinicius/js/main.js
@@ -1,3 +1,14 @@
 function clickBotao(){
-    alert("JOSIAS")
+    let lega = document.getElementById('lucas-vinicius')
+    console.log(lega);
+    lega.className = "mudar-cor"
+
+    let forno = document.getElementById('monfredo')
+    console.log(forno)
+    forno.className = "vermelho"
+
+    let mongol = document.getElementById('francisco')
+    console.log(mongol)
+    forno.classname = "azul"
 }
+   
